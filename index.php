0fcad57ffbbc057b70b9e014bca8d0a0e9b58eea
Boris Morales <bomoral@microsoft.com>
2021-11-25 00:32:14 -0600

    test feature

--- index.php
+++ index.php
@@ -10,6 +10,20 @@ $hello = "Dev Environment";
 
 echo '<b>' . $hello  . '</b>';
 
+//testing new 8 feature
+
+function days_in_month(string $month): int
+{
+    return match(strtolower(substr($name, 0, 3))) {
+        'apr', 'jun', 'sep',  'nov'  => 30,       
+        'jan', 'mar', 'may', 'jul', 'aug',  'oct', 'dec'  => 31,
+        'feb' => is_leap($year) ? 29 : 28,
+        default => throw new InvalidArgumentException("Bogus month"),
+    };
+}
+
+echo 'Days in ' . days_in_month('dec') . '.';
+
 ?>
 
 </body>
